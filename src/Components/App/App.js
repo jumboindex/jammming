85ad3793d6a34219f9d7c5c973808cf630aa7d39
James <ball.j.work@gmail.com>
2021-10-21 15:15:24 +0100

    search / playlist rending data, addTrack() to playlist working

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,20 +2,65 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+                  searchResults: [
+                    { name:'zungguzungguguzungguzeng ',
+                      artist: 'Yellow Man',
+                      album: 'Zungguzungguguzungguzeng!',
+                      id: '1'
+                    }, 
+                    { name:'zungguzungguguzungguzeng ',
+                    artist: 'Yellow Man',
+                    album: 'Zungguzungguguzungguzeng!',
+                    id: '2'}
+                  ],
+                  playlistName: "Jim's playlist",
+                  playlistTracks: [ 
+                    { name:'Morning Ride',
+                      artist: 'Yellow Man',
+                      album: 'Zungguzungguguzungguzeng!',
+                      id: '5'
+                    }, 
+                    { name:'Morning Ride',
+                      artist: 'Yellow Man',
+                      album: 'Zungguzungguguzungguzeng!',
+                      id: '6'}
+                    ]
+                  }
+    this.addTrack = this.addTrack.bind(this);              
+
+  }
+  addTrack(track) {
+    let playlist = this.state.playlistTracks;
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } else {
+      playlist.push(track);
+      return this.setState({playlistTracks: playlist});
+    }
+  }
+
+
+
+render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistname={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } 
 }
 
 export default App;
